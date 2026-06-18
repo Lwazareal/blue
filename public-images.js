52c3043e81ab8e6414b9d544c38294f987fbc952
Lwazareal <leroymalwaza613@gmail.com>
2026-06-18 14:10:52 +0300

    Update public-images.js

--- public-images.js
+++ public-images.js
@@ -5,7 +5,7 @@ window.__PORTFOLIO_IMAGES__ = [
   { type: "image", src: "digital marketig poster.png", alt: "sample 2" },
   { type: "image", src: "Photography Videography.png", alt: "Author image" },
   { type: "image", src: "creative minds (1).jpeg", alt: "Author image" },
-
+  { type: "image", src: "FEASSA - Copy.png", alt: " " },
   { type: "image", src: "samosa world.png", alt: "sample 3" },
   { type: "image", src: "LIQUOR 1.png", alt: "Author image" },
   { type: "image", src: "LIQUOR 2.png", alt: "sample 2" },
@@ -33,36 +33,5 @@ window.__PORTFOLIO_IMAGES__ = [
 
 ];
 
-window.__PORTFOLIO_IMAGES__ = [
-  { type: "image", src: "Digital advertising.png", alt: "Author image" },
-  { type: "image", src: "digital marketig poster.png", alt: "sample 2" },
-  { type: "image", src: "Photography Videography.png", alt: "Author image" },
-  { type: "image", src: "creative minds (1).jpeg", alt: "Author image" },
 
-  { type: "image", src: "samosa world.png", alt: "sample 3" },
-  { type: "image", src: "LIQUOR 1.png", alt: "Author image" },
-  { type: "image", src: "LIQUOR 2.png", alt: "sample 2" },
-  { type: "image", src: "Maridadi Mockup.png", alt: "Author image" },
-  { type: "image", src: "Maridadi-01.png", alt: "sample 3" },
-  { type: "image", src: "Maridadi-02.png", alt: "sample 3" },
-  { type: "image", src: "Maridadi-03.png", alt: "sample 3" },
-  { type: "image", src: "Maridadi-05.png", alt: "sample 3" },
-  { type: "image", src: "miti [Recovered]-03.png", alt: "sample 3" },
-  { type: "image", src: "miti [Recovered]-01.png", alt: "sample 3" },
-  { type: "image", src: "miti [Recovered]-04.png", alt: "sample 3" },
-  { type: "image", src: "valentines (1).png", alt: "sample 3" },
-  { type: "image", src: "valentines (2).png", alt: "sample 3" },
-  { type: "image", src: "valentines (3).png", alt: "sample 3" },
-  { type: "image", src: "valentines (4).png", alt: "sample 3" },
-  { type: "image", src: "Style yetu.png", alt: "sample 3" },
-  { type: "image", src: "Ramadhan poster 2.png", alt: "sample 3" },
-  { type: "image", src: "POST1.png", alt: "sample 3" },
-  { type: "image", src: "POST2.png", alt: "sample 3" },
-  { type: "image", src: "ROCK BOTTOM podcast-02.png", alt: "sample 3" },
-  { type: "image", src: "Garissa Enterprices.png", alt: "sample 3" },
-  { type: "image", src: "Garissa Enterprices mockup1.png", alt: "sample 3" },
-  { type: "image", src: "Lwaza Creatives Logo.png", alt: "sample 3" },
-  { type: "image", src: "koli logo 2.png", alt: "sample 3" },
-
-];
 
